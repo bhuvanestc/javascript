1bc6044237e23a64f7a6c5d25051fef42797396e
Bhuvaneswari <130910193+bhuvanestc@users.noreply.github.com>
2023-12-26 15:00:16 +0100

    Create person_object.js

---
+++ person_object.js
@@ -0,0 +1,10 @@
+
+let person = {
+    name: "John Doe",
+    age: 30,
+    address: "123 Main St, City, Country"
+};
+
+console.log(`Name: ${person.name}`);
+console.log(`Age: ${person.age}`);
+console.log(`Address: ${person.address}`);
